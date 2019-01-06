16c57cac50239c0e236efa27415bbd2d4c5ba25e
Cyril Gabathuler <cyril@3ap.ch>
2019-01-06 22:44:51 +0100

    Assignments week 2

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -17,7 +17,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
+    derivate = (X' * (X * theta - y)) / m;
+    theta = theta - alpha * derivate;
 
 
 
